2c3df291b6bb877d80372c46bc822605ed791512
Raeed <raeedasif1@gmail.com>
2018-12-18 13:47:40 +0500

    TCP URL downloader added

--- client.py
+++ client.py
@@ -1,11 +1,14 @@
 from socket import *
 from sys import *
+from select import*
+
 resumeFlag=False
 numberOfConnections=None
 intervalMetricReport=None
 connectionType=None
 fileAddress=None
 outputLocation=None
+
 def parseArguments(argv):
     for i in range(len(argv)):
         if argv[i]=="-r":
@@ -32,13 +35,39 @@ def parseArguments(argv):
             global outputLocation
             outputLocation=argv[i+1]
             print("This is location on client where file is downloaded: "+outputLocation)
-            
-            
-            
-def connectionEstablishment
-    
+
+def TCP_connection(fileAddress):
+    serverPort = 80
+    string = fileAddress
+    split1 = string.split('//')
+    string1 = split1[1]
+    split2 = string1.split('/')
+    split3 = split2[1].split('.')
+    domain = split2[0]
+    file = split3[0]
+    ext = split3[1]
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect((domain, 80))
+    output = 'GET /%s.%s HTTP/1.0\r\n\r\n' % (file,ext)
+    s.sendall(output.encode())
+    reply = b''
+
+    while True:
+        data = s.recv(2048)
+        if not data: break
+        reply += data
+
+    headers = reply.split(b'\r\n\r\n')[0]
+    image = reply[len(headers) + 4:]
+
+    # save image
+    f = open('%s.%s' % (file, ext), 'wb')
+    f.write(image)
+    f.close()
+
+    s.close()
+
+
 if __name__ == '__main__':
     parseArguments(argv)
-    connectionEstablishement()
-
-    
+    TCP_connection(fileAddress)
